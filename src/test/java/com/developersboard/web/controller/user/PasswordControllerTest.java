944a8d99aba312f67a54d77a78a6d6990dfa9be8
rabiaa <rb618118@dal,ca>
2023-11-04 18:21:52 -0300

    feat: adding tests again

--- src/test/java/com/developersboard/web/controller/user/PasswordControllerTest.java
+++ src/test/java/com/developersboard/web/controller/user/PasswordControllerTest.java
@@ -1,15 +1,25 @@
 package com.developersboard.web.controller.user;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import com.developersboard.backend.service.user.UserService;
 import com.developersboard.constant.user.PasswordConstants;
+import com.developersboard.shared.dto.UserDto;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.ui.Model;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @ExtendWith(MockitoExtension.class)
 class PasswordControllerTest {
@@ -37,5 +47,24 @@ class PasswordControllerTest {
     mockMvc
         .perform(MockMvcRequestBuilders.post(PasswordConstants.PASSWORD_RESET_ROOT_MAPPING))
         .andExpect(MockMvcResultMatchers.status().is4xxClientError());
+  }
+
+  @Mock UserService userService;
+  @Mock PasswordEncoder passwordEncoder;
+
+  @Test
+  void changePasswordCorrectCredentials() {
+    Model model = mock(Model.class);
+    RedirectAttributes redirectAttributes = mock(RedirectAttributes.class);
+    UserDto userDto = new UserDto();
+    userDto.setUsername("Raven");
+    userDto.setPassword("sammy48384fireBond");
+
+    when(userService.findByUsername("Raven")).thenReturn(userDto);
+    when(passwordEncoder.matches("sammy48384fireBond", userDto.getPassword())).thenReturn(true);
+
+    String result = passwordController.changePassword(userDto, model, redirectAttributes);
+
+    assertEquals(PasswordConstants.PASSWORD_RESET_COMPLETE_VIEW_NAME, result);
   }
 }
